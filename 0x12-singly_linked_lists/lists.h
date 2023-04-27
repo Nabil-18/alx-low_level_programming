d89eff85505f598e94a3231638b315df48d969cd
Nabil-18ls <nabilbouhmaladev@gmail.com>
2023-04-27 04:02:36 -0700

    tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -22,5 +23,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 
 #endif
