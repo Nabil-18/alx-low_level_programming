d4cbdb11b3450a4708fa9ea121747293f0376bb1
Nabil-18ls <nabilbouhmaladev@gmail.com>
2023-04-27 04:54:25 -0700

    tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,8 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 typedef struct list_s
 {
 	char *str;
