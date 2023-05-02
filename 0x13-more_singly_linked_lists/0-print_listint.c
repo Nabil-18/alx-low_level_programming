bc558a29ad5591fb26b20f2ef2c8164c16cf6e5e
Nabil-18ls <nabilbouhmaladev@gmail.com>
2023-05-02 03:13:08 -0700

    tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,19 +3,19 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * Return: the number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+    size_t nodes_num = 0;
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
-	}
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        nodes_num++;
+        h = h->next;
+    }
 
-	return (num);
+    return (nodes_num);
 }
+
